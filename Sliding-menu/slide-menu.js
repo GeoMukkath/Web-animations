e531e2528511d112d44e23e60173e83fddea4f82
Geo Mukkath <geomukkath@yandex.com>
2018-01-06 18:59:14 +0530

    Add files via upload

---
+++ Sliding-menu/slide-menu.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+$('#hamburger').click(function(){
+
+$('#mainmenu').animate({'left':'0px'},200);
+
+
+});
+
+$('#close').click(function(){
+ $('#mainmenu').animate({'left':'-300px'},200)
+
+});
+
+});
